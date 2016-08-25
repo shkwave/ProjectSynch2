e57a80f96f451a9cedc61e26233006a9ae0deb43
shkwave <f.naddei@gmail.com>
2016-08-25 23:30:07 +0200

    small clear up
    eliminated useless bit of code at the end of AttackMenuScript.gd

--- game/Scripts/Menu/AttackMenuScript.gd
+++ game/Scripts/Menu/AttackMenuScript.gd
@@ -1,8 +1,7 @@
 
 extends Control
 
-func _ready():
-	pass
+onready var ActDef = get_node("/root/ActionClassDefinition")
 
 func menu_generate(active_hero):
 	var attackoptions = active_hero.get_attack_options()
@@ -12,7 +11,6 @@ func menu_generate(active_hero):
 		s.set_size(Vector2(120,30))
 		s.set_text(attackoptions[i])
 		s.set_name(attackoptions[i])
+		s.connect("pressed",ActDef,"createaction",[attackoptions[i],get_tree().get_root().get_node("World").ACTIVE_CHAR])
 		self.add_child(s)
-	
-
 
