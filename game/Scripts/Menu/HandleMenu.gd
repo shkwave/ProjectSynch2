8cf30603355eca5e1aba3598c8ede37f785c799b
shkwave <f.naddei@gmail.com>
2016-09-24 17:18:05 +0200

    Area of Effect Visualization
    - When selecting the target of an action, now, the area of effect is
    visualized before clicking on a specific point in the selectable area
    - When selecting the target of an action after a first selection is
    done, the target can be changed before confirming it without having to
    go back with the cancel button
    - Tested some color combinations

--- game/Scripts/Menu/HandleMenu.gd
+++ game/Scripts/Menu/HandleMenu.gd
@@ -19,14 +19,6 @@ func _unhandled_input(event):
 			var selected_pos = TERRAIN.world_to_map(get_global_mouse_pos())
 			if selected_pos == WORLD.ACTIVE_CHAR.get_grid_pos():
 				WORLD.change_state("Action_Select")
-#			for Character in WORLD.get_node(WORLD.ACTIVE_PLAYER).get_children():
-#				if (Character.get_grid_pos() == selected_pos):
-#					if (WORLD.get_state() == "Action_Select"):
-#						WORLD.change_state("Char_Select")
-#					Character.change_state("ACTIVE")
-#					WORLD.ACTIVE_CHAR = Character
-#					WORLD.change_state("Action_Select")
-
 	
 	if(event.is_action_pressed("ui_cancel")):
 		if (WORLD.get_state()=="Action_Select" and MENU.currentMenu == MENU.MenuList["MainMenu"]):
@@ -63,29 +55,50 @@ func _input(event):
 				get_tree().set_input_as_handled()
 				if (selected_pos == ActSelReq.reply):
 					ActSelReq.requesting_action.accept_reply(ActSelReq.reply)
+					set_process(false)
+				elif (ActSelReq.requesting_action.global_selectable_area.find(selected_pos)>-1):
+					ActSelReq.reply = selected_pos
+					update()
+
+func _process(delta):
+	var selected_pos = TERRAIN.world_to_map(get_global_mouse_pos())
+	ActSelReq.draw_temp = false
+	if ActSelReq.requesting_action.global_selectable_area.find(selected_pos)>-1:
+		ActSelReq.draw_temp = true
+		ActSelReq.temp_reply = selected_pos
+	update()
 
 func _draw():
 	if (WORLD.get_state() == "Action_Specifics"):
 		if (ActSelReq.flag > 0):
 			var rectsize = Vector2(20,20)
 			for tile in ActSelReq.requesting_action.global_selectable_area:
-				draw_rect(Rect2(TERRAIN.map_to_world_centered(tile)-rectsize/2,rectsize),Color(1,0,0,0.3))
+				draw_rect(Rect2(TERRAIN.map_to_world_centered(tile)-rectsize/2,rectsize),Color(1,0.816,0.1216,0.7))
+			if ActSelReq.draw_temp :
+				if (ActSelReq.requesting_action.AoE_rotate):
+					ActSelReq.requesting_action.AoE_update(ActSelReq.temp_reply)
+				for tile in ActSelReq.requesting_action.AoE:
+					draw_rect(Rect2(TERRAIN.map_to_world_centered(tile+ActSelReq.temp_reply)-rectsize/2,rectsize),Color(1,0.341,0.173,0.4))
 		if (ActSelReq.flag == 2):
-			var rectsize = Vector2(30,30)
+			var rectsize = Vector2(12,12)
 			if (ActSelReq.requesting_action.AoE_rotate):
 				ActSelReq.requesting_action.AoE_update(ActSelReq.reply)
 			for tile in ActSelReq.requesting_action.AoE:
-								draw_rect(Rect2(TERRAIN.map_to_world_centered(tile+ActSelReq.reply)-rectsize/2,rectsize),Color(0,1,1,0.9))
-								
+				draw_rect(Rect2(TERRAIN.map_to_world_centered(tile+ActSelReq.reply)-rectsize/2,rectsize),Color(0.91,0.11,0.478,1))
+
 func act_sel_req_reset():
 	ActSelReq.flag = 0
 	ActSelReq.requesting_action = null
+	ActSelReq.draw_temp = false
 	ActSelReq.reply = Vector2()
+	ActSelReq.temp_reply = Vector2()
+	set_process(false)
 	
 func set_target_request(action):
 	WORLD.change_state("Action_Specifics")
 	ActSelReq.flag = 1
 	ActSelReq.requesting_action = action
+	set_process(true)
 	update()
 
 
